b08239dee4aeed6a3b1d71e9bbb2f21fac2a0e39
David Alméciga <wdavid12@gmail.com>
2025-10-08 09:51:47 -0500

    frontend install

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const App = () => {
+
+  return (
+    <Container className="py-4">
+      <Row>
+        <Col>
+          <h1 className="text-center mb-4">Code Challenge</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      clean: true,
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                ['@babel/preset-react', { runtime: 'automatic' }]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        title: 'Code Challenge App'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true
+        }
+      }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map'
+  };
+};
